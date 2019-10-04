465c9b536c4afdeafb5cac18b9daaf7e29a51107
ryanaleksander <ryan.alva.aleksander@gmail.com>
2019-10-04 15:00:13 +0700

    Implement accuracy calculation during training

--- train.py
+++ train.py
@@ -90,6 +90,7 @@ def train(params, train_loader, validation_loader):
         training_loss = 0
         training_losses = []
         training_progress = tqdm(enumerate(train_loader))
+        correct = 0
         for batch_index, (frames, label) in training_progress:
             training_progress.set_description('Batch no. %i: ' % batch_index)
             frames, label = frames.to(device), label.to(device)
@@ -99,12 +100,18 @@ def train(params, train_loader, validation_loader):
             loss = criterion(output, label)
             loss.backward()
             optimizer.step()
-
             training_loss += loss.item()
+
+            # Calculating accuracy
+            _, prediction = output.max(dim=1)
+            correct += prediction.eq(torch.LongTensor(label)).sum()
+
         else:
             avg_loss = training_loss / len(train_loader)
+            accuracy = correct / len(train_loader) * train_loader.batch_size
             training_losses.append(avg_loss)
             print(f'Training loss: {avg_loss}')
+            print(f'Training accuracy': {accuracy:0.2f})
 
         print('*********Validating*********')
         artnet.eval()
@@ -118,12 +125,17 @@ def train(params, train_loader, validation_loader):
 
                 output = artnet(frames)
                 loss = criterion(output, label)
-
                 validating_loss += loss.item()
+
+                # Calculating accuracy
+                _, prediction = output.max(dim=1)
+                correct += prediction.eq(torch.LongTensor(label)).sum()
             else:
                 avg_loss = validating_loss / len(validation_loader)
+                accuracy = correct / len(train_loader) * validation_loader.batch_size
                 validating_losses.append(avg_loss)
-                print(f'Validating loss: {avg_loss}')
+                print(f'Validation loss: {avg_loss}')
+                print(f'Validation accuracy': {accuracy:0.2f})
         print('=============================================')
         print('Epoch %i complete' % (epoch + 1))
 
@@ -134,7 +146,7 @@ def train(params, train_loader, validation_loader):
         # Update LR
         scheduler.step()
     print('Training complete, saving final model....')
-    torch.save(artnet.state_dict(), os.path.join(params['ckpt_path'], 'arnet_final'))
+    torch.save(artnet.state_dict(), os.path.join(params['ckpt_path'], 'artnet_final.pth'))
     return training_losses, validating_losses
 
 def save_result(train_losses, val_losses, params):
